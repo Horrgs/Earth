af7c5b1815c6059f131a2968eb045b1ecebe5200
MJ <Horrgs@users.noreply.github.com>
2022-06-20 01:27:18 -0400

    Update req.py
    Issue: (1) Headers are 'set' in the method get(). These headers are specific to NWS and are redundant (or may cause issues) when using other services. This needs to be removed. Maybe **kwargs or *args?

--- utils/req.py
+++ utils/req.py
@@ -24,59 +24,26 @@ class RequestMethod(enum.Enum):
     DELETE = "DELETE"  # send HTTP request via DELETE method.
 
 
-"""
-method: send - send HTTP request using specifying parameters. 
-
-parameter: url - the URL string destination for the HTTP request, value of a string (e.g. 'http://www.google.com')
-parameter: method - the HTTP method to use in the HTTP request, value of a RequestMethod enum (e.g. RequestMethod.GET)
-parameter: payload - 
-"""
-
-# TODO: check what data is in request.
-# TODO: check if 'try' can end at specified point.
-# TODO: see if it's possible to remove first if statement.
-def send(url, method: RequestMethod, payload=None):
-    try:
-        # prepare & send HTTP request.
-
-        # TODO: see if it's possible to remove if statement.
-        if payload is not None:  # check if a payload is specified for HTTP request.
-            req = requests.request(method.value, url, data=payload)  # valid payload to send with HTTP request.
-        else:
-            req = requests.request(method.value, url)  # no payload to send, send remaining info.
-        # print(req.text)
-        # TODO: check if try can end here.
-
-        # load response from HTTP request.
-
-        """response = {}  # create empty dict for the response from the request.
-        for item in json.loads(req.text):  # loop over each item in the Response from the Request.
-            response[item['Text']] = int(item['Value'])  # place item in the response dict.
-        response = dict(sorted(response.items(), key=lambda obj: obj[1]))  # sort response dict based on X.
-        return response  # return response dict. """
-        print(req.headers)
-        return req.json()
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
-    except ValueError as e:
-        print(e)
-
 # method to check if web API is available. 200 HTTP is 'OK' status.
 def check_status(url, method: RequestMethod):
     pass
 
 
-# TODO: create cache system.
-
-
-def get(url, method: RequestMethod):
+# TODO: check what data is in request.
+# TODO: check if 'try' can end at specified point.
+# TODO: see if it's possible to remove first if statement.
+def get(url, method: RequestMethod, payload=None):
     headers = {
         'User-Agent': 'https://www.github.com/Horrgs/Earth',
         'Feature-Flag': 'forecast_temperature_qv'
     }
     try:
-        req = requests.request(method=method.value, url=url, headers=headers)  # prepare & send HTTP request.
+        if payload is not None:  # check if a payload is specified for HTTP request.
+            req = requests.request(method=method.value, url=url, headers=headers, data=payload)  # valid payload to send with HTTP request.
+        else:
+            req = requests.request(method.value, headers=headers, url=url)  # no payload to send, send remaining info.
         # TODO: check if try can end here.
+
         if req.status_code != 200:
             pass  # raise error of bad response.
         return req.json()  # returns json (dict) formatted response of request.
@@ -86,8 +53,4 @@ def get(url, method: RequestMethod):
         print(e)
 
 
-response = get('https://api.weather.gov/gridpoints/TOP/31,80/forecast', RequestMethod.GET)
-print(response)
-
-p = GridpointForecastGeoJson.from_json(json.dumps(response))
-print(p.properties.periods)
+# TODO: create cache system.
