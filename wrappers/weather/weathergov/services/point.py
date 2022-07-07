28522f1c8fc6ce0425b6314e1f32da872fb7fcbc
MJ <Horrgs@users.noreply.github.com>
2022-07-07 01:33:59 -0400

    Update point.py to meet NWS schema.
    Close to completing NWS backend. Touch up on notes, type hints, and a few other details are needed. Next to move onto a medium & front-end for service register, user, locations and their fields.
    Knowing the lat long is vital for NWS Point and that can help plenty with fetching other data.
    Need caching.

--- wrappers/weather/weathergov/services/point.py
+++ wrappers/weather/weathergov/services/point.py
@@ -1,32 +1,30 @@
-from dataclasses import dataclass
-from utils.req import send, RequestMethod
+from utils.req import req, RequestMethod
+from dataclasses import dataclass, field
+from dataclasses_json import LetterCase, config, dataclass_json
+from typing import Optional, List, Dict
 
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Point:
-    pass
+    context: None = field(metadata=config(field_name='@context'))  # JsonLdContext. ['@context']
+    id: str = field(metadata=config(field_name='@id'))
+    type: str = field(metadata=config(field_name='@type'))
+    cwa: str  # three letter identifier of the NWS office.
+    forecast_office: str  #
+    grid_id: str  # three letter identifer of the NWS office.
+    grid_x: int  # x-coordinate in the grid from the NWS office
+    grid_y: int  # y-coordinate in the grid from the NWS office.
+    forecast: str  # uri to the GridpointForecast of the specified point.
+    forecast_hourly: str  # uri to the GridpointForecastHourly of the specified point.
+    forecast_grid_data: str  # uri to the raw GridpointForecast data of the specified point.
+    observation_stations: str  # uri to the relevant observation stations
+    relative_location: None
+    forecast_zone: str  # uri to the relevant forecast zone
+    county: str  # uri to the relevant county zone
+    fire_weather_zone: str  # uri to the relevant fire weather zone.
+    time_zone: str  # time zone of specified point.
+    radar_station: str  # radar provider of specified point.
+    geometry: Optional[str] = None  # GeometryString. NULLABLE.
 
-
-def get_grid_info(point):
-    url = "https://api.weather.gov/points/{0},{1}"
-    point = [str(p).strip() for p in point]
-    url = url.format(*point)
-    push_req = send(url, RequestMethod.GET)
-    push_req = push_req['properties']
-    response = {
-        'gridId': push_req['gridId'],
-        'gridX': push_req['gridX'],
-        'gridY': push_req['gridY']
-    }
-    return response
-
-
-def get_weather(point):
-    url = "https://api.weather.gov/gridpoints/{0}/{1},{2}/forecast"
-    grid_info = get_grid_info(point)
-    url = url.format(grid_info['gridId'], grid_info['gridX'], grid_info['gridY'])
-    push_req = send(url, RequestMethod.GET)
-    print(push_req)
-
-
-get_weather([42.8864, -78.8784])
 
