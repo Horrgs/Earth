8f13e2bb4425dddb9233611001b43353262d9862
MJ Horrigan <horrgs34@gmail.com>
2023-02-21 10:26:54 -0500

    Startup & services.

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import config
+import json
+from user.services import Service
 
 
 def get_users():
     pass
 
 
-def get_services():
-    pass
-
-
-def setup(first_run=True):
-    # create services/settings file
+def setup():
+    config.create_config_files()  # Will create any config files that are missing.
     # configure services & settings
     # optionally create an account. If not, skip X step(s).
     # if created a profile, select locations to monitor and modify their settings.
     # if no profile created, use default settings and look up a location.
 
 
-    pass
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    setup()
--- user/services.py
+++ user/services.py
@@ -1,1 +1,34 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json, LetterCase
+import config
+import json
+
+
+@dataclass_json(letter_case=LetterCase.SNAKE)
+@dataclass
+class Service:
+    name: str  # name of the service (i.e. weather.gov)
+    endpoint: str # endpoint for the services' API.
+    enabled: bool  # method for enabling/disabling a servce
+    api_key: str = None # api key for the service. None if no API key required.
+
+
+def load_services():  # function that gets all enabled services and loads them
+    config_files = config.get_config_files()  # get config files
+    if 'config.json' not in config_files:  # check if config.json exists and create if necessary.
+        config.create_config_files()  # create config files. it will only create what's missing (i.e. config.json)
+        raise Exception("config.json not in {0}. Creating file(s).".format(config.get_earth_directory()))
+
+    enabled_subservices = []  # create empty list to store enabled services
+    with open(config_files['config.json'], mode='r') as config_file:  # open config.json
+        service_data = json.load(config_file)['services']  # load json data into dict and select services
+        for service, subservices in service_data.items(): # loop over each service and a list of their subservices
+            enabled_subservices.extend([subservice for subservice in subservices if subservice["enabled"]])  # add subservices that are enabled
+    config_file.close()  # close config.json
+
+    for subservice in enabled_subservices:
+        print(Service.from_dict(subservice))
+
+
+def register_key(service_name):
+    pass
