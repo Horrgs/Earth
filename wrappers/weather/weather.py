2fc28eea1d8e94c2bcf92d5ca899a12d247fd2c9
MJ Horrigan <horrgs34@gmail.com>
2023-02-20 23:49:26 -0500

    weather.gov front end methods.

--- wrappers/weather/weather.py
+++ wrappers/weather/weather.py
@@ -2,6 +2,7 @@ from wrappers.weather.weathergov.services.gridpoint import GridpointForecastGeoJ
 from utils.req import req, RequestMethod
 from wrappers.weather.weathergov.services.point import PointGeoJson
 
+
 def get_location_metadata(location):  # retrieve the grid info of a location in lat-lng, comma separated str format. ('39,-40')
     # Preparing 'payload'
 
@@ -14,11 +15,13 @@ def get_location_metadata(location):  # retrieve the grid info of a location in 
     return PointGeoJson.from_json(metadata.content)  # return Point data for location
 
 
-
 def get_weather(point: PointGeoJson):  # get GridpointForecastGeoJson obj for given PointGeoJson obj.
     point = point.properties  # access the properties field (class type Point)
     forecast_data = req(point.forecast, RequestMethod.GET)  # access Points' forecast URL and send GET req for forecast
     return GridpointForecastGeoJson.from_json(forecast_data.content)  # return Foreceast as GridpointForecstGeoJson
 
 
-print("2")
+weather = get_weather(get_location_metadata('42.8864,-78.8784'))
+
+for period in weather.properties.periods:
+    print("{0}: {1}".format(period.name, period.detailed_forecast))
