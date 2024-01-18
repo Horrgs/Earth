5b4a61ba277a7e186cb870cef37c78725d2371ce
MJ Horrigan <horrgs34@gmail.com>
2024-01-18 00:18:13 -0500

    Progress towards startup and updating User profile.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-import config
+from config import config
+from gui.setup import Setup
 
 
 def get_users():
     pass
 
 
-def setup():
+def startup():
     config.create_config_files()  # Will create any config files that are missing.
-    # configure services & settings
-    # optionally create an account. If not, skip X step(s).
-    # if created a profile, select locations to monitor and modify their settings.
-    # if no profile created, use default settings and look up a location.
+    if config.is_initial_run(): # first time set up
+        Setup() # initiate GUI for first time set up
+
+        # configure services & settings
+        # optionally create an account. If not, skip X step(s).
+        # if created a profile, select locations to monitor and modify their settings.
+        # if no profile created, use default settings and look up a location.
+    else:
+        pass
+
+def login():
+
+
 
 
 if __name__ == '__main__':
-    setup()
+    startup()
